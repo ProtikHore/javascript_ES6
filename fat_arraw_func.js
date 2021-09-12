74bbdef070ba5fe5dffbf91c3180687076bcc127
ProtikHore <protik.hore@gmail.com>
2021-09-12 14:05:02 +0600

    first commit

---
+++ fat_arraw_func.js
@@ -0,0 +1,27 @@
+// normal function
+function number() {
+    return 10;
+}
+
+function number2(n) {
+    return n;
+}
+
+//fat arraw function
+let number3 = () => {
+    return  5;
+}
+
+let number4 = (n2) => {
+    return  n2;
+}
+
+let number5 = () => 2; //jodi function er akta matro statement thake r jodi sei statement ta return kore
+
+console.log(number());
+console.log(number2(5));
+
+
+console.log(number3());
+console.log(number4(20));
+console.log(number5());
