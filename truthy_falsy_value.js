16cfd99cf50f1d5e1e9f2f1bc88daee5a90bd8fc
ProtikHore <protik.hore@gmail.com>
2021-09-12 14:21:39 +0600

    truthy and falsy value

---
+++ truthy_falsy_value.js
@@ -0,0 +1,20 @@
+// explicity false na hoy
+// expliity 0 na hoy
+// blank ba empty string na hoy
+// null na hoy
+// undefined na hoy
+// not a number na hoy
+
+// truthy
+//otherwise falsy
+
+// false, 0, "",null, undefined, Nan hole false....baki true
+
+
+var myVar = "";
+
+if(myVar) {
+    console.log('this is truthy');
+} else {
+    console.log('this is falsy');
+}
